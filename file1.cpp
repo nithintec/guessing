316ef505738f09aafa00ab920239043709a58e6f
Nithin Sekhar <nithinsekhartechosa@gmail.com>
2022-06-17 15:02:49 +0530

    random number generation with apple

--- file1.cpp
+++ file1.cpp
@@ -18,5 +18,73 @@ int main()
 
     cin >> a;
 
+    if (a == 1) {
+        srand(time(0));
+        num = (rand() % 6) + 1;
+
+    }
+
+
+
+    if (num == 1) {
+        set = 1;
+        basket1 = "apple";
+        basket2 = "";
+        basket3 = "";
+        basket4 = "";
+        basket5 = "";
+        basket6 = "";
+
+
+    }
+
+    if (num == 2) {
+        set = 2;
+        basket2 = "apple";
+        basket1 = "";
+        basket3 = "";
+        basket4 = "";
+        basket5 = "";
+        basket6 = "";
+
+    }
+    if (num == 3) {
+        set = 3;
+        basket3 = "apple";
+        basket1 = "";
+        basket2 = "";
+        basket4 = "";
+        basket5 = "";
+        basket6 = "";
+
+    }
+    if (num == 4) {
+        set = 4;
+        basket4 = "apple";
+        basket1 = "";
+        basket2 = "";
+        basket3 = "";
+        basket5 = "";
+        basket6 = "";
+    }
+    if (num == 5) {
+        set = 5;
+        basket5 = "apple";
+        basket1 = "";
+        basket2 = "";
+        basket3 = "";
+        basket4 = "";
+        basket6 = "";
+    }
+    if (num == 6) {
+        set = 6;
+        basket6 = "apple";
+        basket1 = "";
+        basket2 = "";
+        basket3 = "";
+        basket4 = "";
+        basket5 = "";
+    }
+
 }
 
