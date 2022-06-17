a657a4b0014b01693c28af9b8ec2afe1cfbc0967
Nithin Sekhar <nithinsekhartechosa@gmail.com>
2022-06-17 14:57:43 +0530

    initialising variables

--- file1.cpp
+++ file1.cpp
@@ -7,6 +7,16 @@ using namespace std;
 
 int main()
 {
-    
+    string basket1, basket2, basket3, basket4 = "", basket5 = "", basket6 = "";
+
+    int a, num = 0, basketNumber, set = 0;
+
+
+    cout << "WELCOME TO THE GAME" << endl;
+
+    cout << "Press 1 to start game" << endl;
+
+    cin >> a;
+
 }
 
